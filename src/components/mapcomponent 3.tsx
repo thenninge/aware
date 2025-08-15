e3744aa7b945f464286f25af12200a5154cfec48
Tomas Henninge <tomas@Mac.lan>
2025-08-15 15:34:08 +0200

    Chore: Remove all delete _getIconUrl hacks from all mapcomponent files. Only mergeOptions remains for Leaflet icons.

--- src/components/mapcomponent 3.tsx
+++ src/components/mapcomponent 3.tsx
@@ -77,7 +77,6 @@ function MapController({
   useEffect(() => {
     if (typeof window !== 'undefined' && typeof L !== 'undefined') {
       try {
-        delete (L.Icon.Default.prototype as L.Icon.Default.Options)._getIconUrl;
         L.Icon.Default.mergeOptions({
           iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
           iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
